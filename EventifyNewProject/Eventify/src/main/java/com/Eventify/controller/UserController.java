dec49a4a078909d2ee6c12e999df42ccb747e565
Satya2023 <saratkumarsharma01@gmail.com>
2025-05-03 22:17:47 +0530

    CRUD Operation completed for user

--- EventifyNewProject/Eventify/src/main/java/com/Eventify/controller/UserController.java
+++ EventifyNewProject/Eventify/src/main/java/com/Eventify/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.Eventify.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus; 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,5 +39,27 @@ public class UserController {
 		UserDto getUserById=userService.getUserDataById(userId);
 		return  ResponseEntity.ok(getUserById);
 	}
+	
+	//Build Get All User by restApi
+	@GetMapping("/list")
+	public ResponseEntity<List<UserDto>> getAllUser(){
+		List<UserDto> getAllUserList=userService.getAllUser();
+		return  ResponseEntity.ok(getAllUserList);
+	}
+	
+	//Build Update User by restApi
+	@PutMapping("/update/{id}")
+	public ResponseEntity<UserDto> updatedUser(@PathVariable("id") String userId,@RequestBody UserDto updateUserDto){
+		UserDto updatedUserDtoval=userService.updateUserDataById(userId, updateUserDto);
+		return  ResponseEntity.ok(updatedUserDtoval);
+	}
+	
+	//Build Delete User by userID
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<String> deletedUserById(@PathVariable("id") String userId){
+		String str=userService.deleteUserById(userId);
+		return ResponseEntity.ok("Deleted Successfully");
+	}
+	
 
 }
--- EventifyNewProject/Eventify/src/main/java/com/Eventify/service/impl/UserServiceImpl.java
+++ EventifyNewProject/Eventify/src/main/java/com/Eventify/service/impl/UserServiceImpl.java
@@ -1,7 +1,9 @@
 package com.Eventify.service.impl;
 
-import org.springframework.stereotype.Service;
+import java.util.List; 
 
+import org.springframework.stereotype.Service;
+import java.util.stream.Collectors;
 import com.Eventify.Mapper.UserMapper;
 import com.Eventify.dto.UserDto;
 import com.Eventify.entity.User;
@@ -32,6 +34,47 @@ public class UserServiceImpl implements UserService {
 		User user=userrepo.findById(userId).orElseThrow(() -> new ResourceNotFoundException("Given id is not present in database please verify your Id"+userId));
 		UserDto userdto=UserMapper.mappedToUserDto(user);
 		return userdto;
+	}
+
+	@Override
+	public List<UserDto> getAllUser() {
+		// TODO Auto-generated method stub
+		List<User> userList=userrepo.findAll();
+		return userList.stream().map((user) ->UserMapper.mappedToUserDto(user))
+				.collect(Collectors.toList());
+	}
+
+
+	@Override
+	public UserDto updateUserDataById(String userId, UserDto updatedUser) {
+		// TODO Auto-generated method stub
+		User user=userrepo.findById(userId).orElseThrow(() -> new ResourceNotFoundException("Given id is not present in database please verify your Id"+userId));
+		user.setUserId(updatedUser.getUserId());
+		user.setFirstName(updatedUser.getFirstName());
+		user.setLastName(updatedUser.getLastName());
+		user.setMobileNo(updatedUser.getMobileNo());
+		user.setEmail(updatedUser.getEmail());
+		user.setUserName(updatedUser.getUserName());
+		user.setDateOfBirth(updatedUser.getDateOfBirth());
+		user.setGender(updatedUser.getGender());
+		user.setStatus(updatedUser.getStatus());
+		user.setCreatedAt(updatedUser.getCreatedAt());
+		user.setUpdatedAt(updatedUser.getUpdatedAt());
+		user.setPasswordFst(updatedUser.getPasswordFst());
+		user.setProfilePicture(updatedUser.getProfilePicture());
+		
+		User updatedUserObj=userrepo.save(user);
+		UserDto updatedUserDto=UserMapper.mappedToUserDto(updatedUserObj);
+		
+		return updatedUserDto;
+	}
+
+	@Override
+	public String deleteUserById(String userId) {
+		// TODO Auto-generated method stub
+		User user=userrepo.findById(userId).orElseThrow(() -> new ResourceNotFoundException("Given id is not present in database please verify your Id"+userId));
+		userrepo.delete(user);
+		return "Deleted sucessfully";
 	}
 
 }
